2ce25f283360f11b32d6c42a3292a88d09ef9896
夏沫花火zzz <ami@muska.zip>
2024-10-20 07:12:24 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -10,14 +10,14 @@ import pkg from '../package.json'
 export default {
     root: '/',
     package: `${pkg.name}/${pkg.version}`,
-    apiPrefix: 'https://api.iedon.com/dn42/peerapi',
+    apiPrefix: 'https://api.dn42.muska.zip/dn42/peerapi',
     pingIntervalMs: 180000,
     configFallback: {
-        netAsn: '4242422189',
-        netName: 'iedon.dn42',
-        netDesc: 'IEDON-NET',
+        netAsn: '4242423745',
+        netName: 'muska.dn42',
+        netDesc: 'Muska Network - DN42 Network',
         footerText: 'Running in fallback mode',
         maintenanceText: ''
     },
-    gravatarUrlPrefix: 'https://www.gravatar.com/avatar/'
+    gravatarUrlPrefix: 'https://cravatar.cn/avatar/'
 }
